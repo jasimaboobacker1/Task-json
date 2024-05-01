3cf71c44b5386a6e56ed3edd6955c8b8720eb126
unknown <jasimaboobacker1>
2024-05-01 14:09:34 +0530

    message

---
+++ index.js
@@ -0,0 +1,13 @@
+const jsonServer = require('json-server')
+const taskServer = jsonServer.create()
+const middleware = jsonServer.defaults()
+const router = jsonServer.router("db.json")
+const PORT = 3000 || process.env.PORT
+
+taskServer.use(middleware)
+taskServer.use(router)
+
+
+taskServer.listen(PORT,()=>{
+    console.log('Employee portal server started at port 3000');
+})
